1efeb208de39470b3370551531023ab010f9d750
codephillip <codephillip@gmail.com>
2016-01-04 15:25:22 +0300

    refractored code

--- app/src/main/java/com/codephillip/intmain/e_govt/MyIntentService.java
+++ app/src/main/java/com/codephillip/intmain/e_govt/MyIntentService.java
@@ -4,11 +4,7 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
-import com.google.gson.Gson;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -25,22 +21,8 @@ public class MyIntentService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
 
-        List<DataObject> objList = new ArrayList<DataObject>();
-        objList.add(new DataObject("monday", "zero"));
-        objList.add(new DataObject("tuesday", "one"));
-        objList.add(new DataObject("wednesday", "two"));
-
-        // Convert the object to a JSON string
-        String json = new Gson().toJson(objList);
-//        System.out.println(json);
-        Log.d("JSON", json);
-
         try {
-
-//            JSONObject jsonPost = new JSONObject().put("day", "tuesday44");
-//            String jsonString = post("http://192.168.56.1/lynda-php/api.php/api1", String.valueOf(jsonPost));
-//            String jsonString = post("http://192.168.56.1/lynda-php/api.php/api1", "{\"day\":\"thursdaycode\"}");
-            String jsonString = post("http://192.168.56.1/lynda-php/api.php/api1", DataObject.returnJson());
+            String jsonString = post("http://192.168.56.1/lynda-php/api.php/api1", Utility.returnJson());
             Log.d("JSON#", jsonString);
         } catch (Exception e) {
             e.printStackTrace();
---
+++ app/src/main/java/com/codephillip/intmain/e_govt/Utility.java
@@ -0,0 +1,10 @@
+package com.codephillip.intmain.e_govt;
+
+/**
+ * Created by codephillip on 1/4/16.
+ */
+public class Utility {
+    public static String returnJson(){
+        return "{\"day\":\"thursdaycode\",\"lunch\":\"food\"}";
+    }
+}
