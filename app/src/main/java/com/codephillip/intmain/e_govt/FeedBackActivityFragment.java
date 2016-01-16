bdc339b0016fc1eaa9bb9778c35114032b0a7ce1
codephillip <codephillip@gmail.com>
2016-01-16 22:28:32 +0300

    added code to feedback

--- app/src/main/java/com/codephillip/intmain/e_govt/FeedBackActivityFragment.java
+++ app/src/main/java/com/codephillip/intmain/e_govt/FeedBackActivityFragment.java
@@ -2,13 +2,18 @@ package com.codephillip.intmain.e_govt;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,13 +22,15 @@ public class FeedBackActivityFragment extends Fragment {
 
     String intentString;
     boolean intentReceived = false;
+    private TextInputLayout textInputLayoutTopic;
+    private TextInputLayout textInputLayoutMessage;
 
     public FeedBackActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle saveInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_feed_back, container, false);
 
@@ -35,16 +42,37 @@ public class FeedBackActivityFragment extends Fragment {
             e.printStackTrace();
         }
 
-//        FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        Button send = (Button) rootView.findViewById(R.id.send);
+        Button reset = (Button) rootView.findViewById(R.id.reset);
+        final EditText editTopic = (EditText) rootView.findViewById(R.id.editTopic);
+        final EditText editMessage = (EditText) rootView.findViewById(R.id.editMessage);
 
+        textInputLayoutTopic = (TextInputLayout) rootView.findViewById(R.id.editTopicLayout);
+        textInputLayoutMessage = (TextInputLayout) rootView.findViewById(R.id.editMessageLayout);
+        textInputLayoutTopic.setErrorEnabled(true);
+        textInputLayoutMessage.setErrorEnabled(true);
 
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Send: ", "Sending ..");
+
+                if (editTopic.getText().length() <= 0 || editMessage.getText().length() <= 0){
+                    editTextError();
+                }
+                else {
+                    Toast.makeText(getContext(), editTopic.getText() + "#" + editMessage.getText(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTopic.setText("");
+                editMessage.setText("");
+            }
+        });
 
         return rootView;
     }
@@ -61,5 +89,11 @@ public class FeedBackActivityFragment extends Fragment {
             editor.putString(chapterStrip, intentString);
             editor.commit();
         }
+    }
+
+    private void editTextError(){
+        Resources res = getResources();
+        textInputLayoutTopic.setError(res.getString(R.string.topic_required));
+        textInputLayoutMessage.setError(res.getString(R.string.message_required));
     }
 }
