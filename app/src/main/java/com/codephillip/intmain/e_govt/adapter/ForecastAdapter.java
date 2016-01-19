1bb1d719d6e395df4eda2eaccf58415d2b603d2d
codephillip <codephillip@gmail.com>
2016-01-19 22:54:01 +0300

    fixed todayLayout

--- app/src/main/java/com/codephillip/intmain/e_govt/adapter/ForecastAdapter.java
+++ app/src/main/java/com/codephillip/intmain/e_govt/adapter/ForecastAdapter.java
@@ -131,7 +131,8 @@ public class ForecastAdapter extends CursorAdapter {
 
     @Override
     public int getItemViewType(int position) {
-        return (position == 0 && mUseTodayLayout) ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+        return (position == 0) ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+//        return (position == 0 && mUseTodayLayout) ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
     }
 
     @Override
