8a6ac905d0958a12e29951793036a715da2e2b8a
codephillip <codephillip@gmail.com>
2016-01-21 23:34:34 +0300

    added likebutton lib

--- app/src/main/java/com/codephillip/intmain/e_govt/sync/SyncAdapter.java
+++ app/src/main/java/com/codephillip/intmain/e_govt/sync/SyncAdapter.java
@@ -79,9 +79,9 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
             e.printStackTrace();
         }
 
-        String baseUrl = "10.10.9.245";
+//        String baseUrl = "10.10.9.245";
 //        String baseUrl = "192.168.43.243";
-//        String baseUrl = "192.168.56.1";
+        String baseUrl = "192.168.56.1";
         try {
             int k;
             for (k=0; k<4 ; k++){
